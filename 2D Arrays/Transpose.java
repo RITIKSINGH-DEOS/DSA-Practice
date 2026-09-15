8ac9e4d12ff11691f12710e9fda704d04837d229
RIITK SINGH <businessritiksinghdeos@gmail.com>
2026-09-15 19:43:50 +0530

    Updated 2D Arrays sheet with more questions

---
+++ 2D Arrays/Transpose.java
@@ -0,0 +1,28 @@
+class Transpose {
+    public static void main(String[] args) {
+
+        int[][] matrix = {
+            {1, 2, 3},
+            {4, 5, 6}
+        };
+
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+
+        int[][] transpose = new int[cols][rows];
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+
+                transpose[j][i] = matrix[i][j];
+            }
+        }
+
+        for (int i = 0; i < transpose.length; i++) {
+            for (int j = 0; j < transpose[i].length; j++) {
+                System.out.print(transpose[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ Day3/flr.java
@@ -0,0 +1,17 @@
+package Day3;
+
+public class flr {
+
+    public static int sum(int n) {
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            sum = sum + i;
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        int x = sum(5);
+        System.our.println(x);
+    }
+}
